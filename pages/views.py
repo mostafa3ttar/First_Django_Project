162be56348680bacf08413a275a5e77001cbe49b
Mostafa Ali <captain.desha55@gmail.com>
2026-03-03 21:16:12 +0200

    step1: linked the pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    x = {'name':'mostafa ali taha',
+         'age':255}
+    return render(request, 'pages/index.html', x)
+    # return HttpResponse('index page')
+
+
+def about(request):
+    y = {'name':'mostafa ali taha',
+         'age':255}
+    return render(request, 'pages/about.html', y)
+    # return HttpResponse('About page')
