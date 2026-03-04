27cfa6a5ab53b77d010448dfe95bba187e4cf8ca
Mostafa Ali <captain.desha55@gmail.com>
2026-03-04 15:00:21 +0200

    made static folder and URL routing

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=3)
