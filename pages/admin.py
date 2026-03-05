a4ce78096b346b8ef76d0ceec750002516e02685
Mostafa Ali <captain.desha55@gmail.com>
2026-03-05 21:10:02 +0200

    linking models and add products was done

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Female,Male, User, Product,UserName,Video
 # Register your models here.
+
+admin.site.register(Female)
+admin.site.register(Male)
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(UserName)
+admin.site.register(Video)
---
+++ products/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def product(request):
+    return render(request, 'products/product.html')
+
+
+def products(request):
+    return render(request, 'products/products.html', {'pro' : Product.objects.all()})
+
